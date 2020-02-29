c54df77a4f88442aa137fcadb0aec8b387aae972
Caleb Kavanaugh <calebck@hotmail.com>
2020-02-29 01:57:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {(<div>"I am very happy right now!"</div>)}} />
   </div>,
   document.getElementById('root')
 );
